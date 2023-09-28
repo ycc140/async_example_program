d03dd64cc22753e43f9ecbb4ed0784ddfb682363
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2023-09-28 23:15:05 +0200

    Updaterd documentation.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # noinspection StructuralWrap
 """
-.. image:: /static/async_template_prog.png
+.. image:: https://github.com/ycc140/async_example_program/blob/main/static/async_template_prog.png
    :width: 95%
 
 This async template program contains a number of goodies when you are building a lot of microservice programs.
